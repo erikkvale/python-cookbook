c7874e377421892cf4e3804a28aca3ac3179bb29
Erik Kvale <eirikval@gmail.com>
2018-06-27 22:46:03 -0500

    Wrote and passed doctest

--- Ch08_classes_and_objects/802_customizing_str_formatting.py
+++ Ch08_classes_and_objects/802_customizing_str_formatting.py
@@ -1,3 +1,9 @@
+"""
+>>> d = Date(2018, 7, 1)
+>>> format(d)
+'2018-7-1'
+"""
+
 _formats = {
     'ymd': '{d.year}-{d.month}-{d.day}',
     'mdy': '{d.month}/{d.day}/{d.year}',
@@ -7,13 +13,13 @@ _formats = {
 
 class Date:
 
-    def __init__(self, year, month, date):
+    def __init__(self, year, month, day):
         self.year = year
         self.month = month
-        self.date = date
+        self.day = day
 
     def __format__(self, code):
-        if code = '':
+        if code == '':
             code = 'ymd'
         fmt = _formats[code]
         return fmt.format(d=self)
